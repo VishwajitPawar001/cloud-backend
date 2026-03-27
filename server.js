efb1d1ff544d0ff32c0282004a493e9e0aa42f86
Vishwajit Pawar <pawarvishwajit675@gmail.com>
2026-03-28 00:04:42 +0530

    fix folder routes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-
 const express = require("express");
 const cors = require("cors");
 
@@ -8,7 +7,6 @@ const authRoutes = require("./routes/authRoutes");
 const fileRoutes = require("./routes/fileRoutes");
 const folderRoutes = require("./routes/folderRoutes");
 const shareRoutes = require("./routes/shareRoutes");
-
 
 const app = express();
 
@@ -23,11 +21,11 @@ app.use("/api/folders", folderRoutes);
 app.use("/api/share", shareRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Cloud Storage API Running");
+  res.send("Cloud Storage API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
